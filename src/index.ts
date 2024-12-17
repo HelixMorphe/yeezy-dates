90bce16c4384ccda4e1175e9d965c760e42cb30d
saisanthoshpentakota <saisanthosh.pentakota@contentstack.com>
2024-12-17 22:54:59 +0530

    Setup GetSuggestions method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export function Init() {}
+import { parseDate } from 'chrono-node';
+
+import {
+  getRelativeSuggestions,
+} from './engines/relative-time-engine/relative-time-engine';
+import { Suggestion } from './types';
+
+export function getSuggestions(input: string): Suggestion[] {
+  const relativeTimeSuggestions = getRelativeSuggestions(input);
+  if (relativeTimeSuggestions.length === 0) {
+    const date = parseDate(input);
+    if (date === null) {
+      return [];
+    }
+    return [{ label: input, date: date }];
+  }
+
+  return relativeTimeSuggestions;
+}
